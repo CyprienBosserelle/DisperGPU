fe941ceefee93cc4aa74d35a6863e284299792c3
Cyprien <cyprien.bosserelle@gmail.com>
2024-10-19 20:18:04 +1300

    Create TriMesh.cu

---
+++ DisperGPU/TriMesh.cu
@@ -0,0 +1,33 @@
+//////////////////////////////////////////////////////////////////////////////////
+//DispROMS_GPU                                                                    //
+//Copyright (C) 2013 Bosserelle                                                 //
+//                                                                              //
+//This program is free software: you can redistribute it and/or modify          //
+//it under the terms of the GNU General Public License as published by          //
+//the Free Software Foundation.                                                 //
+//                                                                              //
+//This program is distributed in the hope that it will be useful,               //
+//but WITHOUT ANY WARRANTY; without even the implied warranty of                //    
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                 //
+//GNU General Public License for more details.                                  //
+//                                                                              //
+//You should have received a copy of the GNU General Public License             //
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.         //
+//////////////////////////////////////////////////////////////////////////////////
+
+// Compute barycentric coordinates (u, v, w) for
+// point p with respect to triangle (a, b, c)
+__device__ __host__ void Barycentric(Point p, Point a, Point b, Point c, float &u, float &v, float &w)
+{
+    Vector v0 = b - a, v1 = c - a, v2 = p - a;
+    float d00 = Dot(v0, v0);
+    float d01 = Dot(v0, v1);
+    float d11 = Dot(v1, v1);
+    float d20 = Dot(v2, v0);
+    float d21 = Dot(v2, v1);
+    float denom = d00 * d11 - d01 * d01;
+    v = (d11 * d20 - d01 * d21) / denom;
+    w = (d00 * d21 - d01 * d20) / denom;
+    u = 1.0f - v - w;
+}
+
